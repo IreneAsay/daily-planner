8f23e446353a77f67c4bcd608c559af505e2dbd9
Irene Asay <ireneasay@Irenes-MacBook-Pro.local>
2020-10-08 06:38:57 +0800

    created index.html, style.css, script.js, README.md files

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# daily-planner
+# Daily Planner
+
+## What is the purpose of this project? 
+
+
+
+![Screenshot 1](assets/)
+
+
+## How to achieve my goal?
+
+
+## Challenges
+
+
+
+
+## Credits
+
+The initial codes are provided by Trilogy Education Services, a 2U, Inc. brand.
+
+
+## License
+
+© Irene Asay. All Rights Reserved. Under the [MIT/](./license.txt) license.
---
+++ assets/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    console.log("Loading!");
+    var dayScheduler = JSON.parse(localStorage.getItem("scheduler"));
+    if(dayScheduler == null) dayScheduler = [];
+    var currentHour = moment().format("h");
+    console.log(dayScheduler);
+    console.log(currentHour)
+    $("#9").addClass("present");
+    // $("#"+currentHour).addClass("present");
+    // $("#"+currentHour).prevAll().addClass("past");
+    // $("#"+currentHour).nextAll().addClass("future");
+    for(var i = 0; i < $(".time-block").length; i++){
+        $(".time-block:eq(" + i + ") .description").val(dayScheduler[i]);
+        
+    }  
+
+    $(".saveBtn").on("click", function() {
+        var scheduler = $(this).siblings(".description").val();
+        // console.log(scheduler);
+        var time = $(this).parent().attr("id");
+        // console.log(time);
+        var index = time - 9;
+        dayScheduler[index] = scheduler;
+        localStorage.setItem("scheduler", JSON.stringify(dayScheduler));
+        // localStorage.setItem(time,value)
+    })
+
+});
+
